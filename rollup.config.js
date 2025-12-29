fcb289ce4ea9345274886ca4c00c17df33d5c20b
冰龍與雪風的工作室 <57035251+SetsuikiHyoryu@users.noreply.github.com>
2025-12-29 11:15:29 +0900

    chore: make rollup could be used in Mac

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import typescript from '@rollup/plugin-typescript'
+import { defineConfig } from 'rollup'
 
-export default {
+export default defineConfig({
   input: 'src/main.ts',
   output: {
     file: './dist/main.mjs',
     format: 'es',
     sourcemap: 'inline',
   },
+  external: (id) => id.startsWith('game'),
   plugins: [typescript()],
-}
+})
